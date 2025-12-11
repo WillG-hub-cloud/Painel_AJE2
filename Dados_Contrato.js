3e827359422814c691d64a61f49b6333c571ab4b
WillG-hub-cloud <williang.egidio@gmail.com>
2025-12-11 09:45:42 -0300

    Update Dados_Contrato.js

--- Dados_Contrato.js
+++ Dados_Contrato.js
@@ -178,6 +178,7 @@ const projectData = [
             previstoDataSets: [
                 { label: "Cronograma Inicial", data: [292590.76, 534282.82, 632255.10, 726776.13, 906496.89, 1078035.34, 1375896.09, 1731815.40, 1939154.42, 2286895.77, 2770035.72, 3062222.23, 3398805.76, 3629613.36] }, 
                 { label: "Cronograma Prorrogação I", data: [null, null, null, 453476.70, 682448.69, 1005437.35, 1308972.20, 1665743.54, 2226698.41, 2584618.50, 3011727.49, 3401989.43, 3592627.37, 3625286.12] }, // Nova série azul (reajuste 1)
+                { label: "Cronograma Prorrogação II", data: [null, null, null, null, null, null, null, null, null, null, null, null, null, null] }
                 ],
             executado: [311389.54, 311389.54, 311389.54, 501587.58, null, null, null, null, null, null, null, null, null, null],
         },
@@ -234,20 +235,15 @@ const projectData = [
         ],
         grafico: { // Populated based on medicoes
             labels: ["Jul/25", "Ago/25", "Set/25", "Out/25", "Nov/25", "Dez/25", "Jan/26"],
-            previstoDataSets: [
-                { label: "Cronograma Inicial", data: [149571.75, 730716.85, 1300261.90, 1480000.00, null, null, null] }, 
-                { label: "Cronograma Prorrogação I", data: [null, 318610.50, 368781.58, 866856.13, 1169484.80, 1333153.20, 1480000.00] }, // Nova série azul (reajuste 1)
-                ],
+            previsto: [149571.75, 318610.50, 368781.58, 866856.13, 1169484.80, 1333153.20, 1480000.00],
             executado: [261030.50, 261030.50, 261030.50, 366229.87, 553163.84, null, null],
+            previstoDataSets: null
         },
         predicted_start_date: '07/07/2025', // OS 9/2025 emitida em 24/06/2025
         predicted_end_date: '06/02/2026', // 120 dias corridos de execução segundo a OS
         location: { lat: -23.184126, lng: -46.853513 }, 
     }
 ];
-
-
-
 
 
 
